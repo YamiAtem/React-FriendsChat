db55b19f2022b183b3809730ae10a182abac5754
Atharva Mishra <atharvamishra956@gmail.com>
2021-03-29 18:17:46 +0530

    added sign in

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -4,15 +4,19 @@ import '../App.css';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-firebase.initializeApp({
-    apiKey: "AIzaSyDJeXM8vrUsbWouls2qmXlKHkwsMwpNB4U",
-    authDomain: "formdom-a2e7e.firebaseapp.com",
-    databaseURL: "https://formdom-a2e7e.firebaseio.com",
-    projectId: "formdom-a2e7e",
-    storageBucket: "formdom-a2e7e.appspot.com",
-    messagingSenderId: "263022664524",
-    appId: "1:263022664524:web:7102e17aba2a411c908de9"
-})
+if (!firebase.apps.length) {
+    firebase.initializeApp({
+        apiKey: "AIzaSyDJeXM8vrUsbWouls2qmXlKHkwsMwpNB4U",
+        authDomain: "formdom-a2e7e.firebaseapp.com",
+        databaseURL: "https://formdom-a2e7e.firebaseio.com",
+        projectId: "formdom-a2e7e",
+        storageBucket: "formdom-a2e7e.appspot.com",
+        messagingSenderId: "263022664524",
+        appId: "1:263022664524:web:7102e17aba2a411c908de9"
+    });
+} else {
+    firebase.app();
+}
 
 const auth = firebase.auth();
 
