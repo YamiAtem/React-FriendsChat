d1a928c9510e86528efed22349e2e8f0f83c0023
Atharva Mishra <atharvamishra956@gmail.com>
2021-03-29 16:10:18 +0530

    fixed firebase default app error

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import React, { useRef, useState } from 'react';
-import './App.css';
+import "./App.css";
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/analytics';
 
+import SignIn from "./components/SignIn";
+
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
-  apiKey: "AIzaSyDJeXM8vrUsbWouls2qmXlKHkwsMwpNB4U",
-  authDomain: "formdom-a2e7e.firebaseapp.com",
-  databaseURL: "https://formdom-a2e7e.firebaseio.com",
-  projectId: "formdom-a2e7e",
-  storageBucket: "formdom-a2e7e.appspot.com",
-  messagingSenderId: "263022664524",
-  appId: "1:263022664524:web:7102e17aba2a411c908de9"
-})
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyDJeXM8vrUsbWouls2qmXlKHkwsMwpNB4U",
+    authDomain: "formdom-a2e7e.firebaseapp.com",
+    databaseURL: "https://formdom-a2e7e.firebaseio.com",
+    projectId: "formdom-a2e7e",
+    storageBucket: "formdom-a2e7e.appspot.com",
+    messagingSenderId: "263022664524",
+    appId: "1:263022664524:web:7102e17aba2a411c908de9"
+  });
+} else {
+  firebase.app();
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
-const analytics = firebase.analytics();
-
 
 function App() {
 
@@ -32,11 +36,13 @@ function App() {
     <div className="App">
       <header>
         <h1>⚛️🔥💬</h1>
-        <SignOut />
+        {/*<SignOut />*/}
       </header>
       <section>
-        {user ? <ChatRoom /> : <SignIn />}
+        <SignIn />
       </section>
     </div>
   );
 }
+
+export default App;
